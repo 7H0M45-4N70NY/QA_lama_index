2bdb45e544ab819b579c8e255734a0138291a1d2
7H0M45-4N70NY <thomasantony14@gmail.com>
2024-02-11 15:45:25 +0530

    file setup coding

---
+++ webpagereader.py
@@ -0,0 +1,19 @@
+from llama_index.llms import Gemini
+from llama_index.readers import SimpleWebPageReader
+from llama_index import VectorStoreIndex
+import llama_index
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main(url:str)->None:
+    document=SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
+    index=VectorStoreIndex.from_documents(documents=document)
+    query_engine=index.as_query_engine()
+    response=query_engine.query("What are the main features mentioned in this blog")
+    print(response)
+
+
+if __name__ == "__main__":
+    main(url="https://blog.llamaindex.ai/llamaindex-gemini-8d7c3b9ea97e")
